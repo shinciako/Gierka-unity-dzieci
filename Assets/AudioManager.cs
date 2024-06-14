ec1bf478b66af31f0dc730e588540c3704430b19
shinciako <100320142+shinciako@users.noreply.github.com>
2024-06-14 05:47:10 +0200

    Add music

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public AudioSource musicSource;
+
+    public static AudioManager instance;
+
+    private void Awake(){
+        if (instance == null){
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else{
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start(){
+        musicSource.clip = bg;
+        musicSource.Play();
+    }
+}
